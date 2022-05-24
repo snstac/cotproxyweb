9b0adb3d4f909ba3a75198b3f6928a94fa39d9d1
Greg Albrecht <github.com@undef.net>
2022-05-24 15:09:41 -0700

    db fixes

--- cotproxyweb/settings.py
+++ cotproxyweb/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'cotproxyweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': "/".join([BASE_DIR, 'db.sqlite3']),
+        'NAME': "/".join([str(BASE_DIR), 'db.sqlite3']),
     }
 }
 
