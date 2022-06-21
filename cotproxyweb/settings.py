ef905852467e52ba6574ac6f2a8b574f64b7606c
Greg Albrecht <github.com@undef.net>
2022-06-20 23:33:47 -0500

    added another allowed host

--- cotproxyweb/settings.py
+++ cotproxyweb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!^f6gcub^b7dy979509djug=7%9d+^3x49b)o84n(y5d&94%*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.17.2.100', '127.0.0.1', 'rorqual.local', 'localhost', '172.17.2.135']
+ALLOWED_HOSTS = ['172.17.2.100', '127.0.0.1', 'rorqual.local', 'localhost', '172.17.2.135', '5fadca038be6b80f99a0d038dc79aa8b.balena-devices.com']
 
 
 # Application definition
